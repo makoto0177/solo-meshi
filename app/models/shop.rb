ba8b041dd756f42d1c93b620807382da3adb963e
makoto <0bv3981478w2t8p@gmail.com>
2025-12-17 21:40:55 +0900

    feat: Post/Shop/Userモデルにバリデーションとアソシエーションを追加

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,25 @@
 class Shop < ApplicationRecord
+  has_many :posts, dependent: :destroy
+  
+  validates :name, presence: true, length: { minimum: 1, maximum: 100 }
+  validates :area, presence: true, length: { minimum: 1, maximum: 50 }
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
+
+  CATEGORIES = [
+    '和食',
+    '洋食',
+    'イタリアン',
+    '中華',
+    'ラーメン',
+    'そば・うどん',
+    '寿司',
+    '焼肉・焼鳥',
+    'カレー',
+    '定食屋',
+    '牛丼・丼もの',
+    'カフェ',
+    '居酒屋',
+    'ファストフード',
+    'その他'
+  ].freeze
 end
