94d66a8b4a95d1c6acab64ac6a5933037512ee1d
makoto <0bv3981478w2t8p@gmail.com>
2025-12-20 16:48:45 +0900

    feat:　PostsControllerと投稿フォームの雛形を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: "pages#home"
+
+  resources :posts, only: [:new, :create]
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
