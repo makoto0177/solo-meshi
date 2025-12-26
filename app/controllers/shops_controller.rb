41d9474d5857a64740e236988cbf6b2cbef21517
makoto <0bv3981478w2t8p@gmail.com>
2025-12-26 12:27:09 +0000

    feat: 店舗の新規作成・編集機能を実装

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,9 +1,51 @@
 class ShopsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_shop, only: [:show, :edit, :update, :destroy]
+
   def index
     @shops = Shop.all.order(created_at: :desc)
   end
 
   def show
+  end
+
+  def new
+    @shop = Shop.new
+  end
+
+  def create
+    @shop = Shop.new(shop_params)
+
+    if @shop.save
+      redirect_to @shop, notice: "店舗を登録しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @shop.update(shop_params)
+      redirect_to @shop, notice: "店舗情報を更新しました"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @shop.destroy
+    redirect_to shops_path, notice: "店舗を削除しました"
+  end
+
+  private
+
+  def set_shop
     @shop = Shop.find(params[:id])
+  end
+
+  def shop_params
+    params.require(:shop).permit(:name, :area, :category, :address, :description)
   end
 end
